2a9c2f70ba989a4e73b6b9148da8173077d7491d
Yusman Kamaleri <ybkamaleri@gmail.com>
2021-08-25 19:29:46 +0200

    Update

---
+++ docs/2021-08-24-orgdata/setup.R
@@ -0,0 +1,12 @@
+# Core Packages -----------------------------------------------------------
+install.packages(c("xaringan", "metathis"))
+install.packages("xaringanthemer", dependencies = TRUE)
+
+
+# Extra Packages ----------------------------------------------------------
+install.packages("remotes")
+remotes::install_github("gadenbuie/xaringanExtra", force = TRUE)
+
+install.packages("fontawesome")
+
+remotes::install_github("helseprofil/orgdata")
